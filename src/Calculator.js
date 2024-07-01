e022929c02213c0508cd2001c283058d447cb40e
ujjwalg123 <ujjwalk.ug21.cs@nitp.ac.in>
2024-07-01 22:47:08 +0530

    first commit

---
+++ src/Calculator.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import './Calculator.css';
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleButtonClick = (value) => {
+    setInput((prevInput) => prevInput + value);
+  };
+
+  const handleClear = () => {
+    setInput('');
+    setResult('');
+  };
+
+  const handleDelete = () => {
+    setInput((prevInput) => prevInput.slice(0, -1));
+    setResult('');
+  };
+
+  const handleCalculate = () => {
+    try {
+      setResult(eval(input).toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  return (
+    <div>
+    <h1 className='heading'>Calculator</h1>
+    <div className="calculator">
+      <div className="display">
+        <input type="text" value={input} readOnly />
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleButtonClick('1')}>1</button>
+        <button onClick={() => handleButtonClick('2')}>2</button>
+        <button onClick={() => handleButtonClick('3')}>3</button>
+        <button className="button-plus" onClick={() => handleButtonClick('+')}>+</button>
+        <button onClick={() => handleButtonClick('4')}>4</button>
+        <button onClick={() => handleButtonClick('5')}>5</button>
+        <button onClick={() => handleButtonClick('6')}>6</button>
+        <button onClick={() => handleButtonClick('7')}>7</button>
+        <button onClick={() => handleButtonClick('8')}>8</button>
+        <button onClick={() => handleButtonClick('9')}>9</button>
+        <button onClick={() => handleButtonClick('-')}>-</button>
+        <button onClick={() => handleButtonClick('.')}>.</button>
+        <button onClick={() => handleButtonClick('0')}>0</button>
+        <button onClick={handleDelete}>Del</button>
+        <button onClick={() => handleButtonClick('*')}>*</button>
+        <button onClick={handleCalculate}>=</button>
+        <button onClick={handleClear}>C</button>
+
+        <button onClick={() => handleButtonClick('00')}>00</button>
+
+        <button onClick={() => handleButtonClick('/')}>/</button>
+  
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Calculator;
